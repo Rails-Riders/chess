7631e8428524205f197b356dd701880748f4897b
rob perez <ptelerez@cox.net>
2015-07-21 20:01:52 +0000

    implemented devise gem

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'bootstrap-sass', '~> 3.3.4'
 
+gem 'devise', '>= 3.2.4'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Chess::Application.routes.draw do
+  devise_for :users
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
