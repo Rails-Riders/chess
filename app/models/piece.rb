ebb97663cccd57f86facf03b52756464c9388f6b
CodeFlowed <syncstep@gmail.com>
2015-07-28 00:59:07 +0000

    Add piece table and serialize the square_location field.

---
+++ app/models/piece.rb
@@ -0,0 +1,6 @@
+class Piece < ActiveRecord::Base
+  serialize :square_location
+
+  belongs_to :user
+  belongs_to :game
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150728002542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pieces", force: true do |t|
+    t.string   "type"
+    t.integer  "active"
+    t.text     "square_location"
+    t.integer  "game_id"
+    t.integer  "player_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
