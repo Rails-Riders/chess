799f772fb69a032ace88b46a87ed19919bdb7068
CodeFlowed <syncstep@gmail.com>
2015-07-31 03:23:51 +0000

    Revert test files to default state

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,9 @@
 class Game < ActiveRecord::Base
-
 	belongs_to :user
+
+  after_create :populate_board!
+
+  def populate_board!
+    
+  end
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  # test "should get index" do
-  #   # get :index
-  #   # assert_response :success
-  # end
-  # test "should pass basic test" do
-  # 	# just a dummy test Does it pass
-  # 	# assert true
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+  test "should pass basic test" do
+  	# just a dummy test Does it pass
+  	assert true
+  end
 end
