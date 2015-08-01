9ef90a06a924a74e86b692351b74240515b35948
CodeFlowed <syncstep@gmail.com>
2015-08-01 11:51:59 +0000

    Work in progress to add functionality to populate_board! method

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,54 @@
 class Game < ActiveRecord::Base
 	belongs_to :user
+  has_many :pieces
 
   after_create :populate_board!
 
   def populate_board!
-    
+    # Store the game's id in a var. Note: .pluck returns an array
+    game_id = Game.pluck(:id)
+
+    # These are initialized to the white pieces' y coordinates.
+    # Note: y2 is for the pawns' y coordinate.
+    y = 1
+    y2 = 2
+
+    # Create white pieces locations on 1st loop and then 
+    # the black pieces locations on the 2nd loop
+    2.times do
+      # Create 8 pawns                   
+      1.upto(8) do |x|
+        Pawn.create( :square_location => [x, y2] )
+      end
+
+      # Create 2 rooks 
+      1.step(8, 7) do |x|
+        Rook.create( :square_location => [x, y] )
+      end
+
+      # Create 2 knights
+      2.step(7, 5) do |x|
+        Knight.create( :square_location => [x, y] )
+      end
+
+      # Create 2 bishops
+      3.step(6, 3) do |x|
+        Bishop.create( :square_location => [x, y] )
+      end
+
+      # Create a queen
+      Queen.create( :square_location => [4, y] )
+
+      # Create a king
+      King.create( :square_location => [5, y] )
+
+      # Update y and y2 values for the black pieces' y coordinates
+      y = 8
+      y2 = 7
+    end  
+
+    # Update pieces with additional attributes
+    Piece.update_all( :active  => 1,
+                      :game_id => game_id[0] )    
   end
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,89 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  test "number of pawns" do
+    game = Game.create
+
+    # puts Piece.where( :type => 'Pawn').inspect
+
+    num_of_pawns = Piece.where( :type => 'Pawn' ).count
+
+    assert_equal 16, num_of_pawns
+  end
+
+  test "number of rooks" do
+    game = Game.create
+
+    # puts Piece.where( :type => 'Rook').inspect
+
+    num_of_rooks = Piece.where( :type => 'Rook' ).count
+
+    assert_equal 4, num_of_rooks
+  end
+
+  test "number of knights" do
+    game = Game.create
+
+    # puts Piece.where( :type => 'Knight').inspect
+
+    num_of_knights = Piece.where( :type => 'Knight' ).count
+
+    assert_equal 4, num_of_knights
+  end
+
+  test "number of bishops" do
+    game = Game.create
+
+    # puts Piece.where( :type => 'Bishop').inspect
+
+    num_of_bishops = Piece.where( :type => 'Bishop' ).count
+
+    assert_equal 4, num_of_bishops
+  end
+
+  test "number of queens" do
+    game = Game.create
+
+    # puts Piece.where( :type => 'Queen').inspect
+
+    num_of_queens = Piece.where( :type => 'Queen' ).count
+
+    assert_equal 2, num_of_queens
+  end
+
+  test "number of kings" do
+    game = Game.create
+
+    # puts Piece.where( :type => 'King').inspect
+
+    num_of_kings = Piece.where( :type => 'King' ).count
+
+    assert_equal 2, num_of_kings
+  end
+
+  test "number of chess pieces is 32" do
+    Game.create
+
+    num_of_pieces = Piece.count
+
+    assert_equal 32, num_of_pieces
+  end
+
+  test "number of pieces with default values" do
+    game = Game.create
+
+    num_of_pieces = Piece.where( :active => 1, :game_id => game.id ).count
+
+    assert_equal 32, num_of_pieces
+  end
+
+  # This test is not able to query for an array
+  # test "white king's square location" do
+  #   game = Game.create
+  #   puts Piece.find_by( :type => 'King').inspect
+  #   num_of_kings = Piece.find_by( :square_location => [5, 1] ).count
+
+  #   assert_equal 1, num_of_kings
   # end
+
 end
