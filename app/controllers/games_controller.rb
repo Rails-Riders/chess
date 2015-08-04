68f34e027c3ec44d7b2c64ac49be1834d8dc393c
vagrant <leamerluck@gmail.com>
2015-08-04 18:32:28 +0000

    Designated row/col in games#show and added 1st Pawn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,27 @@
 class GamesController < ApplicationController
+	def new
+		@game = Game.new
+	end
+
+	def create
+		@game = Game.create(game_params)
+	end
+
 	def show
+		@game = Game.find(params[:id])
+		@pieces = @game.pieces
+		
 	end
   
+  private
+
+  def game
+  	@game ||= Game.where(id: params[:id])
+  end
+
+  def game_params
+  	params.require(:game).permit(
+  		:white_player_id, 
+  		:black_player_id)
+  end
 end
