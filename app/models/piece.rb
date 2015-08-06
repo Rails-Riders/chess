0516f47f6bc37308cd4eeab76783380fa99fe05b
rob perez <ptelerez@cox.net>
2015-08-06 19:42:40 +0000

    implemented is_obstructed method and tests

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,106 @@
 class Piece < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :game
-end
+
+ def is_obstructed?(start_x, start_y, dest_x, dest_y)
+
+ 	#board for testing?
+    board = [
+              ['r','k',nil,'q', 'nil', 'b', 'n', 'r'],
+              ['p', 'p', nil, nil,'p','p','p', 'p'],
+              [nil, nil, nil, nil, nil, nil, nil, nil],
+              [nil, nil, 'p', 'n', nil, nil, nil, nil],
+              [nil, nil, nil, nil, nil, nil, nil, nil],
+              ['b', 'p', nil, nil, nil, nil, nil, 'b'],
+              [nil, nil, 'p', 'p', 'p', 'p', 'p', 'p'],
+              ['r',nil, nil,'q', 'k', 'b', 'n', 'r']
+            ]
+
+    # For horizontal 
+    if start_x == dest_x
+      if start_y < dest_y
+        return true if for_horizontal_movement(start_y, dest_y, start_x, board, 1)
+      end
+      if start_y > dest_y
+        return true if for_horizontal_movement(start_y, dest_y, start_x, board, -1)
+      end
+    end
+
+    #For vertical 
+    if start_y == dest_y
+      if start_x < dest_x
+        return true if for_vertical_movement(start_x, dest_x, start_y, board, 1) 
+      end
+      if start_x > dest_x
+        return true if for_vertical_movement(start_x, dest_x, start_y, board, -1) 
+      end
+    end
+
+    #For diagonal 
+    if start_x != dest_x && start_y != dest_y
+      if start_x < dest_x && start_y < dest_y 
+        return true if for_diagonal_movement_left_to_right(start_x, dest_x, start_y, dest_y, board, 1 )
+      end
+      if start_x < dest_x && start_y > dest_y
+        return true if for_diagonal_movement_right_to_left(start_x, dest_x, start_y, dest_y, board, 1, -1)
+      end
+       if start_x > dest_x && start_y > dest_y
+        return true if for_diagonal_movement_left_to_right(start_x, dest_x, start_y, dest_y, board, -1 )
+      end
+       if start_x > dest_x && start_y < dest_y
+        return true if for_diagonal_movement_right_to_left(start_x, dest_x, start_y, dest_y, board, -1, 1)
+      end
+    end  
+  end#End is_obstructed?
+
+  #For Horizontal
+  def for_horizontal_movement(start_y, dest_y, start_x, board, increment)
+    new_y = start_y
+      while new_y - dest_y != 0
+        new_y = new_y + increment
+        if board[start_x][new_y] != nil
+          return true
+        end
+      end
+  end 
+
+  #For Vertical
+  def for_vertical_movement(start_x, dest_x, start_y, board, increment)
+    new_x = start_x
+     while new_x - dest_x != 0
+       new_x = new_x + increment
+       if board[new_x][start_y] != nil
+         return true
+       end
+     end
+  end
+
+  #For Diagonal Left to Right
+  def for_diagonal_movement_left_to_right(start_x, dest_x, start_y, dest_y, board, increment )
+    new_x = start_x
+      new_y = start_y
+        while new_x - dest_x != 0 && new_y - dest_y != 0
+          new_x = new_x + increment
+          new_y = new_y + increment
+          if board[new_x][new_y] != nil
+            return true
+          end
+        end
+  end
+
+  #For Diagonal Right to Left
+  def for_diagonal_movement_right_to_left(start_x, dest_x, start_y, dest_y, board, increment1, increment2 )
+    new_x = start_x
+      new_y = start_y
+        while new_x - dest_x != 0 && new_y - dest_y != 0
+          new_x = new_x + increment1
+          new_y = new_y + increment2
+          if board[new_x][new_y] != nil
+            return true
+          end
+        end
+  end
+
+
+
+end#End Piece Class
--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -1,7 +1,76 @@
 require 'test_helper'
+require 'minitest/autorun'
 
-class PieceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class PieceTest < MiniTest::Unit::TestCase #ActiveSupport::TestCase
+
+	#Diagonal Tests Left to Right
+    def test_is_obstructed_diagonal_left_to_right_valid
+      assert Piece.new.is_obstructed?(0, 0, 2, 2)
+    end
+
+    def test_is_obstructed_diagonal_left_to_right_invalid
+      assert ! Piece.new.is_obstructed?(0, 1, 2, 3) 
+    end
+
+    def test_is_obstructed_diagonal_reverse_left_to_right_valid
+      assert Piece.new.is_obstructed?(7, 3, 5, 5) 
+    end
+
+    def test_is_obstructed_diagonal_reverse_left_to_right_invalid
+      assert ! Piece.new.is_obstructed?(7, 0, 5, 2) 
+    end
+    
+    #Diagonal Tests Right to Left
+    def test_is_obstructed_diagonal_right_to_left_valid
+      assert Piece.new.is_obstructed?(0, 7, 2, 2) 
+    end
+
+    def test_is_obstructed_diagonal_right_to_left_invalid
+      assert ! Piece.new.is_obstructed?(1, 7, 3, 5) 
+    end
+
+    def test_is_obstructed_diagonal_reverse_right_to_left_valid
+      assert Piece.new.is_obstructed?(7, 7, 5, 5) 
+    end
+
+    def test_is_obstructed_diagonal_reverse_right_to_left_invalid
+      assert ! Piece.new.is_obstructed?(6, 6, 4, 4) 
+    end
+
+    #Vertical Tests
+    def test_is_obstructed_vertical_valid
+      assert Piece.new.is_obstructed?(0, 0, 2, 0) 
+    end
+
+    def test_is_obstructed_vertical_invalid
+      assert ! Piece.new.is_obstructed?(1, 0, 3, 0)
+    end
+
+    def test_is_obstructed_vertical_reverse_valid
+      assert Piece.new.is_obstructed?(7, 3, 5, 3) 
+    end
+
+    def test_is_obstructed_vertical_reverse_invalid
+      assert ! Piece.new.is_obstructed?(5, 1, 3, 1)
+    end
+
+    #Horizontal Tests
+    def test_is_obstructed_horizontal_valid
+      assert Piece.new.is_obstructed?(1, 0, 1, 2) 
+    end
+
+    def test_is_obstructed_horizontal_invalid
+      assert ! Piece.new.is_obstructed?(1, 1, 1, 3)
+    end
+
+    def test_is_obstructed_horizontal_reverse_valid
+      assert Piece.new.is_obstructed?(1, 2, 1, 0) 
+    end
+
+    def test_is_obstructed_horizontal_reverse_invalid
+      assert ! Piece.new.is_obstructed?(6, 2, 6, 0)
+    end
+
+
+  
 end
