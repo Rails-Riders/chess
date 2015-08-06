b967b8b49ce61c6209576521bbb3ac6bae4a7b3e
CodeFlowed <syncstep@gmail.com>
2015-08-06 02:04:27 +0000

    Add tests and logic for king's basic moves
    ------------------------------------------------------------------------
    A King piece now has basic moves assuming it is on a blank chessboard
    with no other chess pieces, and there are validations to ensure it can
    only be updated with appropriate coordinates 1-8, meaning it won't fall
    off the chessboard.

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,3 +1,64 @@
 class King < Piece
+  MIN_POSITION = 1
+  MAX_POSITION = 8
 
+  def move_left
+    return if self.x_position == MIN_POSITION
+    
+    self.update(:x_position => (self.x_position - 1))
+  end
+
+  def move_right
+    return if self.x_position == MAX_POSITION
+    
+    self.update(:x_position => (self.x_position + 1))
+  end
+
+  def move_up
+    return if self.y_position == MAX_POSITION
+    
+    self.update(:y_position => (self.y_position + 1))
+  end
+
+  def move_down
+    return if self.y_position == MIN_POSITION
+    
+    self.update(:y_position => (self.y_position - 1))
+  end
+
+  def move_NE
+    return if self.x_position == MAX_POSITION
+    return if self.y_position == MAX_POSITION
+
+    move_right
+
+    move_up
+  end
+
+  def move_SE
+    return if self.x_position == MAX_POSITION
+    return if self.y_position == MIN_POSITION
+
+    move_right
+    
+    move_down
+  end
+
+  def move_SW
+    return if self.x_position == MIN_POSITION
+    return if self.y_position == MIN_POSITION
+
+    move_left
+    
+    move_down
+  end
+
+  def move_NW
+    return if self.x_position == MIN_POSITION
+    return if self.y_position == MAX_POSITION
+
+    move_left
+
+    move_up
+  end
 end
