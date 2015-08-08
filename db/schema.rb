2fdebbcc7aa82f7aa91b9e9019ba561553fda58f
vagrant <leamerluck@gmail.com>
2015-08-08 04:38:47 +0000

    Rollback schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807201427) do
+ActiveRecord::Schema.define(version: 20150808041256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 20150807201427) do
     t.integer  "x_position"
     t.integer  "y_position"
     t.integer  "color"
-    t.string   "figure"
   end
 
   create_table "users", force: true do |t|
