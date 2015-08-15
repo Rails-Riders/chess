d4b1c7d54d3bf3d2439e7efaa3d02b41c77da1c6
hugolmendez <hugolmendez@aol.com>
2015-08-15 16:09:09 +0000

    correct tests with correct coordinates

--- test/models/knight_test.rb
+++ test/models/knight_test.rb
@@ -3,70 +3,7 @@ require 'test_helper'
 class KnightTest < ActiveSupport::TestCase
 
     test "valid first white knight move" do
-        knight = Piece.create(:type => 'Knight', :x_position => 8, :y_position => 2)
-        
-        expected = true
-
-
-        #tests white moves from starting postion
-        actual = knight.valid_move?(6, 3)
-        assert_equal expected, actual
-
-        #tests white moves from starting postion
-        actual = knight.valid_move?(6, 1)
-        assert_equal expected, actual
-
-        #tests white moves from starting postion
-        actual = knight.valid_move?(7, 4)
-        assert_equal expected, actual
-    end
-
-
-
-    test "valid second white knight move" do
-        knight = Piece.create(:type => 'Knight', :x_position => 8, :y_position => 7)
-        
-        expected = true
-
-
-        #tests white moves from starting postion
-        actual = knight.valid_move?(6, 8)
-        assert_equal expected, actual
-
-        #tests white moves from starting postion
-        actual = knight.valid_move?(6, 6)
-        assert_equal expected, actual
-
-        #tests white moves from starting postion
-        actual = knight.valid_move?(7, 5)
-        assert_equal expected, actual
-    end
-
-
-   
-    test "valid first black knight move" do
-        knight = Piece.create(:type => 'Knight', :x_position => 1, :y_position => 7)
-        
-        expected = true
-
-
-        #tests white moves from starting postion
-        actual = knight.valid_move?(3, 6)
-        assert_equal expected, actual
-
-        #tests white moves from starting postion
-        actual = knight.valid_move?(3, 8)
-        assert_equal expected, actual
-
-        #tests white moves from starting postion
-        actual = knight.valid_move?(2, 5)
-        assert_equal expected, actual
-    end
-
-
-
-    test "valid second black knight move" do
-        knight = Piece.create(:type => 'Knight', :x_position => 1, :y_position => 2)
+        knight = Piece.create(:type => 'Knight', :x_position => 2, :y_position => 1)
         
         expected = true
 
@@ -76,11 +13,74 @@ class KnightTest < ActiveSupport::TestCase
         assert_equal expected, actual
 
         #tests white moves from starting postion
-        actual = knight.valid_move?(3, 1)
+        actual = knight.valid_move?(1, 3)
         assert_equal expected, actual
 
         #tests white moves from starting postion
-        actual = knight.valid_move?(2, 4)
+        actual = knight.valid_move?(4, 2)
+        assert_equal expected, actual
+    end
+
+
+
+    test "valid second white knight move" do
+        knight = Piece.create(:type => 'Knight', :x_position => 7, :y_position => 1)
+        
+        expected = true
+
+
+        #tests white moves from starting postion
+        actual = knight.valid_move?(6, 3)
+        assert_equal expected, actual
+
+        #tests white moves from starting postion
+        actual = knight.valid_move?(8, 3)
+        assert_equal expected, actual
+
+        #tests white moves from starting postion
+        actual = knight.valid_move?(5, 2)
+        assert_equal expected, actual
+    end
+
+
+   
+    test "valid first black knight move" do
+        knight = Piece.create(:type => 'Knight', :x_position => 7, :y_position => 8)
+        
+        expected = true
+
+
+        #tests white moves from starting postion
+        actual = knight.valid_move?(6, 6)
+        assert_equal expected, actual
+
+        #tests white moves from starting postion
+        actual = knight.valid_move?(8, 6)
+        assert_equal expected, actual
+
+        #tests white moves from starting postion
+        actual = knight.valid_move?(5, 7)
+        assert_equal expected, actual
+    end
+
+
+
+    test "valid second black knight move" do
+        knight = Piece.create(:type => 'Knight', :x_position => 2, :y_position => 8)
+        
+        expected = true
+
+
+        #tests white moves from starting postion
+        actual = knight.valid_move?(3, 6)
+        assert_equal expected, actual
+
+        #tests white moves from starting postion
+        actual = knight.valid_move?(1, 6)
+        assert_equal expected, actual
+
+        #tests white moves from starting postion
+        actual = knight.valid_move?(4, 7)
         assert_equal expected, actual
     end
 end
