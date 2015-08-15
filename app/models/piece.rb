c7411fe118d3170a83fb521476d9b709ce2c5e9b
CodeFlowed <syncstep@gmail.com>
2015-08-15 06:21:34 +0000

    Move move_to down in file

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -6,28 +6,6 @@ class Piece < ActiveRecord::Base
     %w(Pawn Rook Knight Bishop Queen King)
   end
 
-  def move_to!(new_x, new_y)
-    friendly_color = self.color
-
-    friend_or_foe = game.pieces.find_by(:x_position => new_x,
-                                        :y_position => new_y)
-
-    # If no piece exists at the new location, just move to said location
-    if friend_or_foe.nil?
-      self.update(:x_position => new_x, :y_position => new_y)
-
-    # Else if a friendly piece exists there, do nothing
-    elsif friend_or_foe.color == friendly_color
-      return
-    else
-      # It's an enemy. Capture it
-      friend_or_foe.update(:active => 0)
-
-      # Move to the enemy's now vacant position
-      self.update(:x_position => new_x, :y_position => new_y)
-    end          
-  end
-
   def is_obstructed?(dest_x, dest_y)
     # Get the piece's starting coordinates
     start_x = self.x_position
@@ -104,4 +82,26 @@ class Piece < ActiveRecord::Base
       return false
     end
   end # of is_obstructed?
+
+  def move_to!(new_x, new_y)
+    friendly_color = self.color
+
+    friend_or_foe = game.pieces.find_by(:x_position => new_x,
+                                        :y_position => new_y)
+
+    # If no piece exists at the new location, just move to said location
+    if friend_or_foe.nil?
+      self.update(:x_position => new_x, :y_position => new_y)
+
+    # Else if a friendly piece exists there, do nothing
+    elsif friend_or_foe.color == friendly_color
+      return
+    else
+      # It's an enemy. Capture it
+      friend_or_foe.update(:active => 0)
+
+      # Move to the enemy's now vacant position
+      self.update(:x_position => new_x, :y_position => new_y)
+    end          
+  end
 end # end class
