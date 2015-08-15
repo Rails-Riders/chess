55336337d8af1a5cf2719f5a26e04ca094afda64
hugolmendez <hugolmendez@aol.com>
2015-08-15 14:33:38 +0000

    correct HEAD conflict

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -2,17 +2,6 @@ require 'test_helper'
 
 class GamesControllerTest < ActionController::TestCase
   test "show board for selected game == 1" do 
-<<<<<<< HEAD
-      @game = Game.create(id: 1)
-      @selected_piece = Piece.where(type: "Queen", color: "1")
-      @pieces = @game.pieces
-      
-      redirect_to :show {controller: :games, id: id}
-
-      expected = Game.find(params[:id])
-
-      assert_template_layout(expected)
-=======
     @game = Game.create(id: 1)
     @selected_piece = Piece.where(type: "Queen", color: "1")
     @pieces = @game.pieces
@@ -22,6 +11,5 @@ class GamesControllerTest < ActionController::TestCase
     expected = Game.find(params[:id])
 
     assert_template_layout(expected)
->>>>>>> c94dcbedf3b511750026a0bdcb7cb93db76fd8ac
   end
 end
