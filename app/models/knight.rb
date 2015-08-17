7084b96eb09b42c42b0f0f2fba6c35fb03c80505
hugolmendez <hugolmendez@aol.com>
2015-08-17 23:13:29 +0000

    Refactor knight method without private section

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -3,13 +3,7 @@ class Knight < Piece
   def valid_move?(new_x, new_y)
     false if x_position == new_x
     false if y_position == new_y
-    right_move?(new_x, new_y)
-  end
 
-  private
-
-  def right_move?(new_x, new_y)
     (x_position - new_x).abs + (y_position - new_y).abs == 3
   end
-
 end
