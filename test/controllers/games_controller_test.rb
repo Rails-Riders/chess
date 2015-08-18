571b75a8369b4ba27054c209817e043882d262f8
hugolmendez <hugolmendez@aol.com>
2015-08-18 02:53:30 +0000

    correct games#controller test

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -1,15 +1,11 @@
 require 'test_helper'
 
 class GamesControllerTest < ActionController::TestCase
-  test "show board for selected game == 1" do 
-    @game = Game.create(id: 1)
-    @selected_piece = Piece.where(type: "Queen", color: "1")
-    @pieces = @game.pieces
-    
-    redirect_to :show {controller: :games, id:}
-
-    expected = Game.find(params[:id])
-
-    assert_template_layout(expected)
+  test "show page for game == 1" do 
+      @game = Game.create(id: 1)
+      @selected_piece = Piece.where(type: "Queen", color: "1")
+      @pieces = @game.pieces
+      
+      assert_generates "games/1", {:controller => 'games', :action => 'show', :id => @game.id}
   end
 end
