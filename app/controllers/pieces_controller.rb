39d5761a92069466d9543f4dd1a57ff7381af79a
vagrant <leamerluck@gmail.com>
2015-08-25 19:09:55 +0000

    Added valid_move? to pieces#update to check before update

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,19 +1,31 @@
 require 'pry'
 class PiecesController < ApplicationController
 	before_action :select_pc, :only => [:show, :update]
+	before_action :only => :update do 
+		valid_move?(:x_position, :y_position)
+	end
 
 	def show
 		@pieces = select_pc.game.pieces
 	end
 
 	def update
-		row = params[:y_position]
-		col = params[:x_position]
+		row = params[:y_position].to_i
+		col = params[:x_position].to_i
 		@moved_pc = select_pc.update_attributes(y_position: row, x_position: col)
 		redirect_to game_path(select_pc.game.id)
 	end
 
 	private
+
+	def valid_move?(x_position, y_position)
+		row = params[:y_position].to_i
+		col = params[:x_position].to_i
+		if !select_pc.valid_move?(col, row)
+			flash[:alert] = "That move is not allowed"
+			redirect_to game_path(select_pc.game.id)
+		end
+	end
 
 	def select_pc
 		@select_pc = Piece.find(params[:id])
