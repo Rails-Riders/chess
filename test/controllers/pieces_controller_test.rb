b9a76533ac3da5c2ae6b6c62b85eaa4e88d70c5c
vagrant <leamerluck@gmail.com>
2015-08-26 05:18:39 +0000

    Finalized validate_move to call valid_move? and Bug fixes for view
    Changed method name to validate_move which will call valid_move? for each piece
    Corrected the bugs in view: Changed square link to only visible after piece link
    has been selected. This makes the square not clickable if a piece is not selected
    thus fixing the bug that caused a redirect to an alternate game when an empty
    square was clicked prior to selecting a piece to move.

--- test/controllers/pieces_controller_test.rb
+++ test/controllers/pieces_controller_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
+require 'pry'
 
 class PiecesControllerTest < ActionController::TestCase
-  
-  
+  #test "selected piece is allowed to move if valid_move returns true" do 
+  #	@game = Game.create
+  #	@piece = Piece.find_by(type: "Pawn", color: 1, x_position: 4, y_position: 2)
+  #	dest_x = 4
+  #	dest_y = 3
+  	#binding.pry
+
+  	#@piece.validate_move(dest_x, dest_y)
+  #	flunk
+  	#assert_redirected_to(piece_path(id: @piece.id, x_position: dest_x, y_position: dest_y), method: :put)
+  	#assert_equal expected, actual
+  end
 end
