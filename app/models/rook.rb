4c4e14730c957c5fbe81d6c0f2c963c16e124b16
rob perez <ptelerez@cox.net>
2015-08-27 18:15:39 +0000

    possibly finished valid_move? for Rook

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -2,10 +2,24 @@ class Rook < Piece
 
 	def valid_move?(dest_x, dest_y)
 
+		#can not move to currently occupied square
+    	return false if dest_x == self.x_position && dest_y == self.y_position
+		
+		#no obstructions allowed
 		return false if is_obstructed?(dest_x, dest_y) == true
 
+		#only vertical or horizontal moves allowed
 		return false if (dest_x < self.x_position || dest_x > self.x_position) && (dest_y < self.y_position || dest_y > self.y_position)
 
+		#no vertical moves off the board
+		return false if dest_x < 1 || dest_x > 8
+    	
+    	#no horizontal moves off the board
+    	return false if dest_y < 1 || dest_y > 8
+
+    	#can not move to currently occupied square
+    	return false if dest_x == self.x_position && dest_y == self.y_position
+		
 		return true
 	end
 
--- test/models/rook_test.rb
+++ test/models/rook_test.rb
@@ -6,7 +6,7 @@ class RookTest < ActiveSupport::TestCase
 		pieces_to_deactivate = game.pieces.where.not(:type => 'Rook')
 		pieces_to_deactivate.update_all(:active => 0)
 
-	    # Find any rook
+	    #Find any rook
 	    rook = game.pieces.find_by(:type => 'Rook')
 		rook.update(:x_position => 3, :y_position => 5)
 
@@ -27,14 +27,58 @@ class RookTest < ActiveSupport::TestCase
 		#move south
 		actual = rook.valid_move?(8, 5)
 		assert_equal expected, actual
+	end
+
+	test "moves that are invalid" do
+
+		game = Game.create
+		pieces_to_deactivate = game.pieces.where.not(:type => 'Rook')
+		pieces_to_deactivate.update_all(:active => 0)
+
+		# Find any rook
+	    rook = game.pieces.find_by(:type => 'Rook')
+		rook.update(:x_position => 3, :y_position => 5)
+
+		
+		expected = false
+
+		#move north-east
+		actual = rook.valid_move?(1, 7)
+		assert_equal expected, actual
+
+		#move north-west
+		actual = rook.valid_move?(1, 3)
+		assert_equal expected, actual
+
+		#move south-east
+		actual = rook.valid_move?(5, 7)
+		assert_equal expected, actual
+
+		#move south-west
+		actual = rook.valid_move?(5, 3)
+		assert_equal expected, actual
+
+		#move off board east
+		actual = rook.valid_move?(3, 9)
+		assert_equal expected, actual
+
+		#move off board west
+		actual = rook.valid_move?(3, 0)
+		assert_equal expected, actual
+
+		#move off board south
+		actual = rook.valid_move?(9, 5)
+		assert_equal expected, actual
+
+		#move off board north
+		actual = rook.valid_move?(0, 5)
+		assert_equal expected, actual
+
+		#move to current square
+		actual = rook.valid_move?(3, 5)
+		assert_equal expected, actual
 
 
 	end
-
-	#test "moves that are invalid" do
-
-	# 	game = Game.create
-
-	# end
 
 end
