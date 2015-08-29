21994f2b9339c8ff2cc611c0044831ad802bcd41
CodeFlowed <syncstep@gmail.com>
2015-08-29 10:03:32 +0000

    Remove the piece_id column from the piece.rb ------------------------------------------------------------------------ This migration cleans up an unnecessary column in the pieces table that is unecessary because each piece by default has its own id.

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -9,24 +9,33 @@ class GamesControllerTest < ActionController::TestCase
     assert_generates "games/1", {:controller => 'games', :action => 'show', :id => @game.id}
   end
 
-  # test "backend should ignore the waiting player's moves" do
-  #   # Assume that 2 players have joined a game and are assigned their colors
-  #   white_player = FactoryGirl.create(:user, :id => 7)
-  #   black_player = FactoryGirl.create(:user, :id => 3)
+  test "backend should ignore the waiting player's moves" do
+    # Assume that 2 players have joined a game and are assigned their colors
+    white_player = FactoryGirl.create(:user, :id => 7)
+    black_player = FactoryGirl.create(:user, :id => 3)
 
-  #   game = FactoryGirl.create(:game,
-  #                             :id => 10,
-  #                             :player_turn => 1,
-  #                             :white_player_id => 7,
-  #                             :black_player_id => 3)
+    game = FactoryGirl.create(:game,
+                              :id => 10,
+                              :player_turn => 1,
+                              :white_player_id => 7,
+                              :black_player_id => 3)
 
-  #   black_pawn = game.pieces.find_by(:x_position => 1, :y_position => 7)
+    # Assume that the functionality of assigning players their respect pieces
+    # by color is already built and replicate that
+    white_pieces = game.pieces.where(:color => 1).update_all(:player_id => 7)
 
-  #   # Make the black player try to make a move outside of his turn
-  #   put :update, id: black_pawn.id, piece: { x_position: 1, y_position: 6 }
+    black_pieces = game.pieces.where(:color => 0).update_all(:player_id => 3)
 
 
 
-  #   assert true
-  # end
+    black_pawn = game.pieces.find_by(:x_position => 1, :y_position => 7)
+    puts black_pawn.inspect
+
+    # Make the black player try to make a move outside of his turn
+    # put :update, id: black_pawn.id, piece: { x_position: 1, y_position: 6 }
+
+
+
+    assert true
+  end
 end
