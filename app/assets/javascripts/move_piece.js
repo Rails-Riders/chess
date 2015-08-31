0f3d787ce639f43db6baebbf53105518a8326b39
hugolmendez <hugolmendez@aol.com>
2015-08-31 23:32:54 +0000

    js does not work

--- app/assets/javascripts/move_piece.js
+++ app/assets/javascripts/move_piece.js
@@ -1,27 +1,39 @@
-<br />
-<div class="booyah-box col-xs-10 col-xs-offset-1"> 
-  <div class="game-container">
-    <h1>Have Fun</h1>
-    <table class="chess-board">
-      <tbody>
-        <% 8.downto(1) do |row| %>
-          <tr>
-            <% 1.upto(8) do |col| %>
-              <td class= "<%= board_squares(row) %> droppable" data-square-row="<%= row %> " data-square-col="<%= col %>">            
-                <%  @pieces.detect do |piece| %>
-                  <% if piece.x_position == col && piece.y_position == row %>
-                  <div class="draggable piece color_<%= piece.color %>" data-piece-id="<%= piece.id %>" data-piece-starting-row="<%= row %>" data-piece-starting-col="<%= col %>">
-                    <%= piece_color(piece) %>
-                     </div>
-                  <% end %>
-                <% end %>
-              </td>
-            <% end %>
-          </tr>
-        <% end %>
-      </tbody>
-    </table>
-       <br />
-  <%= link_to 'Sign In', new_user_session_path %>
-    </div>
-</div>
+$(document).ready(function() {
+  if ($(".chess-board").length == 0) {
+    return;
+  }
+
+  $( ".draggable" ).draggable();
+
+
+  $( "td" ).droppable({
+    drop: function( event, ui ) { 
+      //these are where the piece is moving to
+      var startRow  = $(this).data("square-row");
+      var startCol  = $(this).data("square-col");
+
+      //these are the draggable pieces
+      var pieceId   = $(ui.draggable).data("piece-id");
+      var newRow    = $(ui.draggable).data("piece-start-row");
+      var newCol    = $(ui.draggable).data("piece-start-col");
+      var piece     = $(ui.draggable);
+      var newSquare = $(this);
+
+      console.debug("The piece to move has an id of: "+pieceId+".");
+      console.debug("The square to move to is on row: "+startRow+" and col: "+startCol+".");
+
+      $.ajax({
+        method: 'put',
+        url: "/pieces/" + pieceId,
+        data: { y_position: startRow, x_position: startCol }
+      }).done(function() {
+        console.debug("it worked! valid move")
+        newSquare.append(piece.css('position','static'));
+      }).fail(function() {
+        // console.debug("the world really really hates me")
+        // move piece back - invalid move
+      });
+      
+    } //drop
+  }); //droppable
+});
