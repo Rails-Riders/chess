aec5810d8ade44e0f8ad2f5a9ba3e708f43d19c4
vagrant <leamerluck@gmail.com>
2015-09-04 03:47:18 +0000

    Removed commented out code piece.rb. Added active to friendly_piece

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -109,15 +109,13 @@ class Piece < ActiveRecord::Base
     friendly_color = self.color
 
     friend_or_foe = game.pieces.find_by(:x_position => new_x,
-                                        :y_position => new_y)
+                                        :y_position => new_y,
+                                        :active => 1)
 
     # If no piece exists at the new location, just move to said location
     if friend_or_foe.nil?
       self.update(:x_position => new_x, :y_position => new_y)
 
-    # Else if a friendly piece exists there, do nothing
-    #elsif friend_or_foe.color == friendly_color
-      #return
     else
       # It's an enemy. Capture it
       friend_or_foe.update(:active => 0)
